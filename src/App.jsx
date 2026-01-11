05b33b2cea6d78b7fce282ee9d6aea354c979bd1
ClaudiaGA <claudia.ga.projects@gmail.com>
2026-01-12 09:49:29 +1100

    Add basic WorkerHome page with layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import Login from "./pages/login"
 import Register from "./pages/Register"
+import WorkerHome from "./pages/WorkerHome"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/worker" element={<WorkerHome />} />
     </Routes>
     </div>
   )
---
+++ src/pages/WorkerHome.jsx
@@ -0,0 +1,14 @@
+import styles from "../styles/WorkerHome.module.css";
+
+function WorkerHome() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1>Worker Home</h1>
+        <p>Casual worker dashboard</p>
+      </div>
+    </div>
+  );
+}
+
+export default WorkerHome;
